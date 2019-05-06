9fa8b1b53967a70c06137c340a20b863325c4105
Floris Chabert <fchabert@nvidia.com>
2019-05-06 13:38:15 -0400

    Comment stuff out

--- pytorch_translate/examples/generate_iwslt14.sh
+++ pytorch_translate/examples/generate_iwslt14.sh
@@ -4,9 +4,9 @@ NCCL_ROOT_DIR="$(pwd)/nccl_2.1.15-1+cuda8.0_x86_64"
 export NCCL_ROOT_DIR
 LD_LIBRARY_PATH="${NCCL_ROOT_DIR}/lib:${LD_LIBRARY_PATH}"
 export LD_LIBRARY_PATH
-wget https://download.pytorch.org/models/translate/iwslt14/data.tar.gz
-tar -xvzf data.tar.gz
-rm -f data.tar.gz
+#wget https://download.pytorch.org/models/translate/iwslt14/data.tar.gz
+#tar -xvzf data.tar.gz
+#rm -f data.tar.gz
 
 python3 pytorch_translate/generate.py \
        "" \
@@ -26,19 +26,19 @@ python3 pytorch_translate/generate.py \
 # | Translated 6750 sentences (152251 tokens) in 37.9s (4018.00 tokens/s)
 # | Generate test with beam=6: BLEU4 = 31.31, 65.7/39.2/25.2/16.6 (BP=0.971, ratio=0.972, syslen=127453, reflen=131152)
 
-python3 pytorch_translate/generate.py \
-       "" \
-       --path model/averaged_checkpoint_best_0.pt:model/averaged_checkpoint_best_1.pt \
-       --source-vocab-file model/dictionary-de.txt \
-       --target-vocab-file model/dictionary-en.txt \
-       --source-text-file data/test.tok.bpe.de \
-       --target-text-file data/test.tok.bpe.en \
-       --unk-reward -0.5 \
-       --length-penalty 0 \
-       --word-reward 0.25 \
-       --beam 6 \
-       --remove-bpe \
-       --quiet
+# python3 pytorch_translate/generate.py \
+#        "" \
+#        --path model/averaged_checkpoint_best_0.pt:model/averaged_checkpoint_best_1.pt \
+#        --source-vocab-file model/dictionary-de.txt \
+#        --target-vocab-file model/dictionary-en.txt \
+#        --source-text-file data/test.tok.bpe.de \
+#        --target-text-file data/test.tok.bpe.en \
+#        --unk-reward -0.5 \
+#        --length-penalty 0 \
+#        --word-reward 0.25 \
+#        --beam 6 \
+#        --remove-bpe \
+#        --quiet
 
 # output should look like:
 # | Translated 6750 sentences (152251 tokens) in 60.2s (2530.11 tokens/s)
